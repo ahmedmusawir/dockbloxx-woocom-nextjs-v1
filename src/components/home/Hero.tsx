b0699716a96348473c123e8d9c72f759a0b898cb
Ahmed Rahat Musawir <ahmed.musawir@gmail.com>
2026-01-19 11:51:41 +0600

    19jan2026 - hero image fix

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,6 +1,7 @@
 import { getImageUrl } from "@/lib/utils";
 import { HomeData } from "@/types/page";
 import Link from "next/link";
+import Image from "next/image";
 import parse from "html-react-parser";
 
 interface Props {
@@ -18,19 +19,19 @@ const Hero = ({ homeData }: Props) => {
     <div className="bg-white">
       <div className="relative">
         <div className="mx-auto max-w-7xl">
-          <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+          <div className="relative z-10 pt-14 xl:w-full xl:max-w-2xl">
             <svg
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
               aria-hidden="true"
-              className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+              className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white xl:block"
             >
               <polygon points="0,0 90,0 50,100 0,100" />
             </svg>
 
-            <div className="relative px-6 py-2 sm:py-4 lg:px-8 lg:py-5 lg:pr-0">
-              <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl pb-10">
-                <div className="hidden sm:mb-10 sm:flex">
+            <div className="relative px-6 py-2 sm:py-4 xl:px-8 xl:py-5 xl:pr-0">
+              <div className="mx-auto max-w-2xl xl:mx-0 xl:max-w-xl pb-10">
+                {/* <div className="hidden sm:mb-10 sm:flex">
                   <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                     Checkout our Blog...{" "}
                     <Link
@@ -41,10 +42,20 @@ const Hero = ({ homeData }: Props) => {
                       Read more <span aria-hidden="true">&rarr;</span>
                     </Link>
                   </div>
-                </div>
+                </div> */}
                 <h1 className="text-pretty text-4xl font-extrabold sm:text-5xl tracking-tight text-blue-600">
                   {sectionData.title}
                 </h1>
+                {/* Tablet-only hero image - shows between sm (640px) and xl (1280px) */}
+                <div className="mt-8 hidden sm:block xl:hidden">
+                  <Image
+                    src="/images/dockbloxx-tablet-header-1600w.jpg"
+                    alt="DockBloxx Tab Hero"
+                    width={800}
+                    height={400}
+                    className="w-full h-auto"
+                  />
+                </div>
                 <section className="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">
                   {parse(sectionData.content)}
                 </section>
@@ -61,11 +72,11 @@ const Hero = ({ homeData }: Props) => {
           </div>
         </div>
 
-        <div className="hidden lg:block bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <div className="hidden xl:block bg-gray-50 xl:absolute xl:inset-y-0 xl:right-0 xl:w-1/2">
           <img
             alt=""
             src={sectionData.image}
-            className="aspect-[3/2] object-cover lg:aspect-auto lg:size-full"
+            className="aspect-[3/2] object-cover xl:aspect-auto xl:size-full"
           />
         </div>
       </div>
